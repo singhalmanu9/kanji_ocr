7084f863263ccac4558a653c778e464d3bca5068
Tim Toombs <Janet@users-MacBook-Pro.local>
2018-11-02 20:41:11 -0700

    updating image_read_test to deal with dupications

--- image_read_test.py
+++ image_read_test.py
@@ -1,5 +1,5 @@
 #encoding char codes :'iso2022_jp'
- 
+from collections import Counter
 import struct
 from PIL import Image
  
@@ -7,14 +7,17 @@ filename = 'data/ETL9B/ETL9B_1'
 
 def make_images(filename):
 	record_size = 576
+	c = Counter()
 	with open(filename, 'rb') as f:
 		for skip in range(0, 121441):
 		    f.seek(skip * record_size)
 		    s = f.read(record_size)
 		    r = struct.unpack('>2H4s504s64x', s)
-		    print(r[0:3], hex(r[1]))
+		    c[hex(r[1])] += 1
+		    if hex(r[1]) == "0x2422":
+		    	print(c[hex(r[1])])
 		    i1 = Image.frombytes('1', (64, 63), r[3], 'raw')
-		    fn = 'img/ETL9B_{xx}_{yy}.png'.format(xx = (r[0]-1)%20+1,yy = hex(r[1])[-4:])
+		    fn = 'img/ETL9B_{xx}_{yy}_{zz}.png'.format(xx = (r[0]-1)%20+1,yy = hex(r[1])[-4:], zz = c[hex(r[1])])
 		    i1.save(fn, 'PNG')
 files = [ 'data/ETL9B/ETL9B_1', 'data/ETL9B/ETL9B_2', 'data/ETL9B/ETL9B_3', 'data/ETL9B/ETL9B_4', 'data/ETL9B/ETL9B_5']
 
