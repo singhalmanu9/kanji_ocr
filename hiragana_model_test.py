4ebf55d5e7750e96dd55904035b323313d3c185e
unknown <timothytoombs@berkeley.edu>
2018-12-16 02:54:01 -0800

    starting to make changes

--- hiragana_model_test.py
+++ hiragana_model_test.py
@@ -3,27 +3,29 @@ from keras.models import model_from_json
 import os
 import pickle
 import numpy as np
-from scipy.misc import imread;
+from scipy.misc import imread
 
 def load_test_images():
-	files = os.listdir('img/hira_test');
+	files = os.listdir('test_data/hira_test')
 	# print(files[:-10])
-	imgs = [imread('img/hira_test/' + _, mode = "L") for _ in files];
-	return np.array(imgs);
+	imgs = [imread('test_data/hira_test/' + _, mode = "L") for _ in files]
+	return np.array(imgs)
 
-file = open('model.json', 'r');
-json_model = file.read();
-file.close();
-model = model_from_json(json_model);
-model.load_weights('model.h5');
+file = open('2018-11-11 20model.json', 'r')
+json_model = file.read()
+file.close()
+model = model_from_json(json_model)
+model.load_weights('model.h5')
 
-img = load_test_images();
-img = img.reshape(img.shape[0], 63, 64, 1);
-print('Loaded test images');
-classes = model.predict(img);
-indices = [np.argmax(prediction) for prediction in classes];
-
+img = load_test_images()
+img = img.reshape(img.shape[0], 63, 64, 1)
+print('Loaded test images')
+classes = model.predict(img)
+#getting top two predictions
+indices = np.array([np.argsort(prediction)[:2] for prediction in classes])
+indices = np.ndarray.flatten(indices)
 with open("jis_code_indices", 'rb') as f:
-	x = pickle.loads(f.read());
-codes = [x[index] for index in indices];
-print(set(codes));
+	x = pickle.loads(f.read())
+codes = [x[index] for index in indices]
+codeset = set(codes)
+print(codeset, "length" ,len(codeset))
---
+++ make_all_dicts.py
@@ -0,0 +1,59 @@
+"""
+currently, only the JIS to UTF8 dictionary exists for hiragana. 
+The goal of this script is to make a JIS to UTF8 dictionary for each character based on radicals.
+This script takes no inputs, but yields the following output:
+
+{"hiragana": {jis:utf8,jis:utf8},
+"katakana": {jis:utf8,jis:utf8},
+radical1: {jis:utf8,jis:utf8},
+radical2: {jis:utf8,jis:utf8},
+....,
+}
+
+where radical1 and radical2 are substituted names for 
+actual strings representing radicals in the radkfile.
+
+This will give sets of images to train nets on such that 
+can have lower dimensional sets can be used to train on 
+data and then output predictions for each radical
+"""
+import pickle
+import codecs
+import json
+with open(file = 'radkfile', encoding = 'euc_jp') as radkstream:
+	radkstring = radkstream.read()
+start_parsing = False
+radical_map = {}
+current_rad = ''
+current_rad_list = []
+radklines = radkstring.splitlines()
+for line in radklines:
+	if(not start_parsing and line[0] == '$'):
+		start_parsing = True
+	if (start_parsing):
+		#adds all kanji with current radical present
+		if (line[0] != '$'):
+			for rad in line:
+				current_rad_list.append(rad)
+		#update radical if necessary
+		else:
+			if current_rad != '':
+				radical_map.update({current_rad : current_rad_list})
+			current_rad_list = []
+			current_rad = line[2]
+			#adds a radical to a stroke_list iff it belongs in the stroke_list
+			#adds a completed stroke_list to the stroke_map
+#taking care of tail end of generation
+radical_map.update({current_rad : current_rad_list})	
+with open('radicalMap', 'w') as outfile:
+	json.dump(radical_map, outfile)
+
+#now to trim off all \u
+with open('radicalMap', 'r') as trimfile:
+	s = trimfile.read()
+new_s = s.replace("\\u","")
+with open('radicalMap', 'w') as outfile:
+	outfile.write(new_s)
+
+
+#TODO make the dictionary. kanji and hiragana will have to be done manually
